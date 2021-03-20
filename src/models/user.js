7b1391ac310c97c752af557bbb469cab5609a694
C-Harshul <chandrasekharharshul@gmail.com>
2021-03-20 11:42:18 +0530

    Password hashing added

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -53,6 +55,47 @@ const userSchema = new mongoose.Schema({
   
 },{
     timestamps : true
+})
+
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    
+    return userObject
+}
+
+
+userSchema.methods.generateAuthtoken = async function () {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()},'UserToken')
+    user.tokens = user.tokens.concat({token}) 
+    await user.save()
+    
+    return token
+}
+
+userSchema.statics.validateCredentials = async(email,password) => {
+   const user = await User.findOne({email})
+   if(!user){
+      throw new Error ('Invalid emailID')
+   }
+   const match = await bcrypt.compare(password,user.password)
+   if(!match) {
+       throw new Error('Incorrect password')
+   }
+
+   return user
+}
+ 
+userSchema.pre('save',async function(next) {
+    const user = this
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password,8)
+    }
+    next()
 })
 
 const User = mongoose.model('User',userSchema)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const User = require('../models/user')
 
 const router = new express.Router()
 
-router.post('/',async(req,res) => {
-    res.send()
+router.post('/new',async(req,res) => {
+  
+    const user = new User(req.body)
+     try {
+     const token = user.generateAuthtoken() 
+     await user.save()
+     res.send({user,tokens})
+    } catch(e) {
+       res.status(500).send(e)
+   }
+})
+
+
+router.get('/me',async(req,res) => {
+    try{
+        const user = User.findOne({email : req.})
+    }
 })
 
 module.exports = router
