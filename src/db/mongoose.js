56308e064babfae65d64fe57915ac525cdd66948
C-Harshul <chandrasekharharshul@gmail.com>
2021-04-02 23:27:34 +0530

    Mailing functionality added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb+srv://Accident_Detection_Backend:Accident_Detection_Backend@cluster0.3czea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ,{
+mongoose.connect('mongodb://127.0.0.1:27017/accident-detector' ,{
     useNewUrlParser :true,
     useCreateIndex : true,
     useUnifiedTopology: true
